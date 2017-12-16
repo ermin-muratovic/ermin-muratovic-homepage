f2b2915ed80542ce9b41e13b1d9cd1998c7d93e1
ermin <ermin.muratovic@gmail.com>
2017-12-16 11:08:42 +0100

    animate expertin-items add contact section

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 function loadImages() {
     var image = document.getElementsByTagName('img');
-    for (var i = 0; i < image.length; i++) {
+    for (var i = 0, n = image.length; i < n; i++) {
         if(image[i].getAttribute('data-src')) {
             image[i].setAttribute('src',image[i].getAttribute('data-src'));
         }
@@ -8,12 +8,84 @@ function loadImages() {
 }
 
 function fadeInElements() {
+    var elements = getElementsWithAttribute('data-animate');
+    for(var i = 0, n = elements.length; i < n; i++) {
+        var offset = parseInt(elements[i].getAttribute('data-animate-offset'))||0;
+        var objectBottom = getPosition(elements[i]).top+offset; //elements.position.top + elements.outerHeight
+        var windowBottom = getScrollPosition() + getWindowHeight();
 
+        if(objectBottom < windowBottom) {
+            elements[i].classList.add("show");
+        } else {
+            elements[i].classList.remove("show");
+        }
+    }
+}
+
+function getElementsWithAttribute(attribute) {
+    var elements = document.getElementsByTagName('*');
+    var matching = [];
+    for(var i = 0, n = elements.length; i < n; i++) {
+        if(elements[i].getAttribute(attribute) != null)
+            matching.push(elements[i]);
+    }
+    return matching;
+}
+
+function scrollToElementById(id) {
+    var element = document.getElementById(id);
+    element.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+}
+
+function closeMenu() {
+    document.getElementById("showmenucheckbox").checked = false;
+}
+
+function getScrollPosition() {
+    return Math.max(document.body.scrollTop, document.documentElement.scrollTop || 0);
+}
+
+function getWindowHeight() {
+    return Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+}
+
+function getPosition(obj) {
+    var curleft = 0, curtop = 0;
+    if (obj.offsetParent) {
+        do {
+            curleft += obj.offsetLeft;
+            curtop += obj.offsetTop;
+        } while (obj = obj.offsetParent);
+        return { left: curleft, top: curtop };
+    }
+    return undefined;
 }
 
 function scrollToMore() {
-    var element = document.getElementById("expertin");
-    element.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+    scrollToElementById("expertin");
+}
+
+function scrollToPortfolio() {
+    closeMenu();
+    scrollToElementById("portfolio");
+}
+
+function scrollToBlog() {
+    closeMenu();
+    scrollToElementById("blog");
+}
+
+function scrollToContact() {
+    closeMenu();
+    scrollToElementById("contact");
+}
+
+function scaleLandingText() {
+    var element = document.querySelector('#banner .text');
+    if(getScrollPosition() > 0)
+        element.classList.add("scale");
+    else
+        element.classList.remove("scale");
 }
 
 window.onload = function() {
@@ -22,4 +94,5 @@ window.onload = function() {
 
 window.onscroll = function(){
     fadeInElements();
+    scaleLandingText();
 };
